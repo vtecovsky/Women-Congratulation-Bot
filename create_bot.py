75489ebe8861940091eafb2b523f63e81cf73452
Saveliy <s.lekhtin@innopolis.university>
2023-03-08 13:52:07 +0300

    changes related with environment variables

--- create_bot.py
+++ create_bot.py
@@ -1,10 +1,12 @@
 from aiogram import Bot
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import Dispatcher
-from config import TOKEN
 import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 storage = MemoryStorage()
 
-bot = Bot(token=TOKEN)
+bot = Bot(os.getenv('TOKEN'))
 dp = Dispatcher(bot, storage=storage)
